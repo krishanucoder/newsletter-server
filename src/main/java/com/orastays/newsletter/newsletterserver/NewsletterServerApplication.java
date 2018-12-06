53dc5ea2dd48c26b5d7ed67a870cf5f8982f5b9c
codingBitsKolkata <45310401+codingBitsKolkata@users.noreply.github.com>
2018-12-06 12:35:48 +0530

    first commit

---
+++ src/main/java/com/orastays/newsletter/newsletterserver/NewsletterServerApplication.java
@@ -0,0 +1,12 @@
+package com.orastays.newsletter.newsletterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsletterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsletterServerApplication.class, args);
+	}
+}
